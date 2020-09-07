54d6efd9071031b06370ba4090bb72b1fdd6be38
Roselyn Guzman <roselyn.1768260@nv.ccsd.net>
2020-09-07 06:51:58 +0000

    Didn't complete the AnalogClock, I was trying to figure out how to solve the problem by hand and putting it into code.

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,12 +7,18 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
+        int angleHour = scanner.nextInput();
+        int angleMinute = scanner.nextInput();
 
         /*
          *  your code goes here
          */
+        hourHand = angleHour / 720;
+        hourDegrees = hourHand * 0.5;
+        minuteHand = angleMinute / 60;
+        minuteDegrees = minuteHand * 6;
 
+        System.out.print(hourDegrees +" "+ minuteDegrees);
         // closing the scanner object
         scanner.close();
     }
